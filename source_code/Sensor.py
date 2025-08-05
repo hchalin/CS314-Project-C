c1e4a19a3a9c8d1f1c41c8831fcedfed0985e3d9
Hayden <hchalin98@gmail.com>
2025-08-05 15:30:26 -0700

    Basic ship movement added!

--- source_code/Sensor.py
+++ source_code/Sensor.py
@@ -21,4 +21,19 @@ class Sensor:
     self.pos = pos            # Position of the Sensor
     self.search_radius = search_radius  # This will be 2 CP's in every direction
 
+    # After sensor creation, start the scan
+    # Then send the data to the celestial map
+    self.scan(self.pos)
 
+  def scan(self, pos: tuple):
+    '''
+      Scan from top left, to bottom right in a 
+    '''
+
+    print('Start scan')
+
+    x: int = self.pos[0]
+    y: int = self.pos[1]
+
+    x_start = (x - 2, y)  # Start scan 2 CPs to the left
+    y_start = (x, y - 2)  # Start scan 2 CPs up 
